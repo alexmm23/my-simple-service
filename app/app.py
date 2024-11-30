725b5104c450bc07ae48290ee7b16d80818ce260
Alex <alejandromontes393@gmail.com>
2024-11-29 19:49:55 -0600

    Add new endpoint

--- app/app.py
+++ app/app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def home():
     return jsonify({"message": "¡Hola desde Flask!"})
 
+@app.route("/saludo/<nombre>")  
+def saludo(nombre):
+    return jsonify({"message": f"¡Hola {nombre}!"})
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
