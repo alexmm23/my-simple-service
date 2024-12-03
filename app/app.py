b876660cfd1ad365feba645d832e5f1ecfad5d3d
Alex <alejandromontes393@gmail.com>
2024-12-02 21:57:37 -0600

    Agregar endpoint de prueba

--- app/app.py
+++ app/app.py
@@ -13,6 +13,16 @@ def saludo(nombre):
 @app.route("/about")
 def about():
     return jsonify({"message": "About Us"})
+@app.route("/suma/<int:num1>/<int:num2>")
+def suma(num1, num2):
+
+    if(num1 < 0 or num2 < 0):
+        return jsonify({"error": "Los números deben ser positivos"})
+    
+    if(type(num1) != int or type(num2) != int):
+        return jsonify({"error": "Los números deben ser enteros"})
+    
+    return jsonify({"resultado": num1 + num2})
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
